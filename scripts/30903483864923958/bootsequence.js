bd47bc6ae2452cbd38a55c00db02cdbc134895a7
root <rhaley@depaul.edu>
2026-09-06 16:57:31 +0000

    update dates

--- scripts/30903483864923958/bootsequence.js
+++ scripts/30903483864923958/bootsequence.js
@@ -1,6 +1,6 @@
 let bootText = "";
 const bootMessages = [
-    "DaemonOS 2026v1",
+    "DaemonOS 2027v1",
     "Copyright (C) DaemonCon",
     "",
     "INITIALIZING SYSTEM: STARTED",
@@ -17,7 +17,7 @@ const bootMessages = [
     "ESTABLISHING SMBv2 WITH FACULTY: SENT",
     "LOADING DAEMONCON INTERFACE: COMPLETED",
     "",
-    "SYSTEM READY. WELCOME TO DAEMONCON 2026."
+    "SYSTEM READY. WELCOME TO DAEMONCON 2027."
 ];
 
 export function typeWriter(text, i, fnCallback) {
